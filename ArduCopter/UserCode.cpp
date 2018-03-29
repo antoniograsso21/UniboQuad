717d1fdff56a668eadae2f86e62c1fe1651aac77
Antonio Grasso <30302557+graitanto21@users.noreply.github.com>
2018-03-30 00:40:37 +0200

    Update UserCode.cpp

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -1,26 +1,25 @@
 #include "Copter.h"
 
 /*
- * @brief Questa funzione muove un servo motore collegato alla pixhawk secondo l'angolo indicato
+ * @brief This function allows to move the servo connected to the Pixhawk (reimplementation of Arduino function)
  * @params
- * channel: canale della pixhawk a cui è collegato il servo motore
- * angle_value: valore dell'angolo a cui si desidera impostare il servo motore
- * angle_min: angolo estremo inferiore della rotazione del servo motore
- * angle_max: angolo estremo superiore della rotazione del servo motore
+ * channel: Pixhawk's channel which the servo is connected to
+ * angle_value: angle value of servo rotation
+ * angle_min: infimum angle value
+ * angle_max: supremum angle value
  * output_min: min pulse width
  * output_max: max pulse width
  */
 static void move_servo(int channel, int angle_value, int angle_min, int angle_max, int output_min, int output_max)
 {
-    // Controllo la correttezza del primo parametro
+    // check first parameter
     if(channel < 0 || channel > 13 ) // 14 canali OUT
         return;
-    // definisco dei valori di interesse
+    // values of interests
     const int rangeIn = angle_max - angle_min;
     const int rangeOut = output_max - output_min;
     const int deltaIn = angle_value - angle_min;
 
-    // definisco costanti matematiche per migliorare l'accuratezza del risultato di divisioni ed approssimazioni
     const int fixedHalfDecimal = 1;
     const int fixedDecimal = fixedHalfDecimal * 2; //2
 
@@ -33,7 +32,7 @@ static void move_servo(int channel, int angle_value, int angle_min, int angle_ma
      * AUX_CH 3 == channels[10]
      * AUX_CH 4 == channels[11]
      */
-    // scrivo sul canale desiderato il valore pwm precedentemente calcolato
+    // write the desired pwm value to the channel which the servo is connected to
     SRV_Channels::srv_channel(channel)->set_output_pwm(pwm_value);
 }
 
@@ -64,14 +63,14 @@ void Copter::userhook_50Hz()
 #define H_REF 150 // parametro h*, altezza di riferimento
 void Copter::userhook_MediumLoop()
 {
-    // Ottengo l'attributo di attitude heading reference system
+    // attitude heading reference system
     AP_AHRS_DCM ahrs_dcm = ahrs;
 
     AP_InertialNav_NavEKF in_nav = inertial_nav;
-    // Ottengo la velocità inerziale
+    // inertial velocity
     const Vector3f &velocity = in_nav.get_velocity();
 
-    // Calcolo le 3 componenti del vettore sb secondo algoritmo PX4Flow
+    // sb vector's components through PX4Flow algorithm (when using PX4Flow)
     double sxb = ahrs_dcm.cos_pitch()*velocity.x*DELTA_TA - ahrs_dcm.sin_pitch()*H_REF;
     double syb = ahrs_dcm.sin_roll()*ahrs_dcm.sin_pitch()*velocity.x*DELTA_TA
             + ahrs_dcm.cos_roll()*velocity.y*DELTA_TA
@@ -80,39 +79,37 @@ void Copter::userhook_MediumLoop()
             - ahrs_dcm.sin_roll()*velocity.y*DELTA_TA
             + ahrs_dcm.cos_roll()*ahrs_dcm.cos_pitch()*H_REF;
 
-    // Calcolo delta in radianti secondo l'algoritmo
+    // delta in radians
     double delta_radians = 0;
     if(szb == 0)
         szb = 0.01;
     delta_radians = atan(sxb/szb);
 
-    // Calcolo l'angolo delta in gradi: (delta_radians * 180) / PI
+    // delta in degrees: (delta_radians * 180) / PI
     int16_t delta_degrees = (delta_radians * 180) / PI;
 
-    // Calcolo il denominatore da cui ricavare lambda
+    // value used to get lambda
     double ratio = sin(delta_radians)*sxb + cos(delta_radians)*szb;
 
-    // Calcolo lambda in radianti secondo l'algoritmo
+    // lambda in radians
     double lambda_radians = 0;
     if(ratio == 0)
         ratio = 0.01;
     lambda_radians = - atan(syb/ratio);
 
-    // Calcolo l'angolo lambda in gradi: (lambda_radians * 180) / PI
+    // lambda in degrees: (lambda_radians * 180) / PI
     int16_t lambda_degrees = (lambda_radians * 180) / PI;
 
-    // Calcolo l'angolo di rotazione del servo 1
+    // rotation angle of the first servo
     delta_degrees = 90 + delta_degrees;
-    // Calcolo l'angolo di rotazione del servo 2
+    // rotation angle of the second servo
     lambda_degrees = 90 + lambda_degrees;
 
-    // Obbligo gli angoli ad avere un valore compreso tra 0 e 180 (nel caso non in cui non lo fossero)
+    // constraint for angles to have a value between 0 and 180 degrees
     delta_degrees = constrain_int16(delta_degrees,0,180);
     lambda_degrees = constrain_int16(lambda_degrees,0,180);
 
-    // Muovo i due servomotori degli angoli desiderati
-    // Per muovere il servo di un angolo x (gradi) bisogna modificare
-    // solo il secondo parametro, impostandolo a x
+    // Move both servos
     move_servo(9,delta_degrees,0,180,715,2345);
     move_servo(10,lambda_degrees,0,180,590,2360);
 
